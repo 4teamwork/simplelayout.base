adf11bced5de1a82612aaf61f41264db8a55573c
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-10-13 09:05:07 +0000

    bug fix /  views.py code clean up

--- simplelayout/base/views.py
+++ simplelayout/base/views.py
@@ -31,6 +31,7 @@ from simplelayout.base.interfaces import ISimplelayoutTwoColumnView, \
                                       ISimpleLayoutBlock, \
                                       ISlUtils
 
+
 class SimpleLayoutView(BrowserView):
     implements(ISimplelayoutView)
 
@@ -39,7 +40,7 @@ class SimpleLayoutView(BrowserView):
         #XXX: use your brains...
         results = [result.getObject() for result in results]
         return results
-    
+
     def renderBlockProvider(self, context):
         name = u'zug.simplelayout.block'
         context = context
@@ -50,33 +51,39 @@ class SimpleLayoutView(BrowserView):
         addTALNamespaceData(provider, context)
         provider.update()
         return provider.render()
-        
+
     def getContentTypes(self):
-        plone_properties = getToolByName(self.context,'portal_properties')
-        plonegov_properties = getattr(plone_properties,'plonegov.website_properties',False)
-        use_types = plonegov_properties and getattr(plonegov_properties,'org_unit_list_types',[]) or []
-        
-        return use_types 
-    
+        plone_properties = getToolByName(self.context, 'portal_properties')
+        plonegov_properties = getattr(
+            plone_properties,
+            'plonegov.website_properties',
+            False)
+        use_types = plonegov_properties and getattr(
+            plonegov_properties,
+            'org_unit_list_types',
+            []) or []
+
+        return use_types
+
     def getContents(self):
 
-        contentFilter = {'portal_type':self.getContentTypes(),
-                         'sort_on':'sortable_title'}
+        contentFilter = {'portal_type': self.getContentTypes(),
+                         'sort_on': 'sortable_title'}
         return self.context.getFolderContents(contentFilter)
 
-    def toggle_align_to_grid(self,new_value='1'):
+    def toggle_align_to_grid(self, new_value='1'):
         """
         toggles alignment to grid
         """
         context = aq_inner(self.context).aq_explicit
 
-        if hasattr(context,'align_to_grid'):
-            self.context.manage_changeProperties({'align_to_grid':new_value})
+        if hasattr(context, 'align_to_grid'):
+            self.context.manage_changeProperties({'align_to_grid': new_value})
         else:
-            context.manage_addProperty('align_to_grid',new_value,'string')
-            
+            context.manage_addProperty('align_to_grid', new_value, 'string')
+
         return 1
-            
+
     @property
     def isSimplelayout(self):
         """returns boolean if sl capable or not
@@ -84,56 +91,58 @@ class SimpleLayoutView(BrowserView):
         context = self.context
         m_tool = getToolByName(context, 'portal_membership')
         capable = ISimpleLayoutCapable.providedBy(context)
-        if m_tool.isAnonymousUser():return False
+        if m_tool.isAnonymousUser():
+            return False
         member = m_tool.getAuthenticatedMember()
         # don't check modify permissions, cause edit is also in use in
         # other sircumstances
         return capable and member
 
 
+class SimpleLayoutBlockView(BrowserView):
+    pass
 
-class SimpleLayoutBlockView(BrowserView):  
-    pass  
-    
+
 class BlockControl(BrowserView):
-    
-    def __call__(self):    
+
+    def __call__(self):
         parent = aq_inner(self.context).aq_parent
         response = self.request.response
 
-        if self.request.has_key('action'):
+        if 'action' in self.request:
             name = 'action'
-        if self.request.has_key('layout'):
+        if 'layout' in self.request:
             name = 'layout'
         if name:
             utilname = 'block-'+name
             try:
-                converter = getUtility(IBlockControl, 
+                converter = getUtility(IBlockControl,
                                        name=utilname)
             except ComponentLookupError:
                 return response.redirect(parent.absolute_url())
-    
+
             converter.update(parent, self.context, self.request)
         return response.redirect(parent.absolute_url())
 
 
 class ChangeDesign(BrowserView):
+
     def __init__(self, context, request):
         self.context = context
-        self.request = request 
-    
+        self.request = request
+
     def setDesignInterface(self, name, blocks=[]):
         """ sets the new view interface """
 
         context = self.context
         context_expl = aq_inner(self.context).aq_explicit
-        
+
         if not blocks:
             #remove grid infos
-            if hasattr(context_expl,'align_to_grid'):
-                context.manage_changeProperties({'align_to_grid':'0'})
-                
-            if not VIEW_INTERFACES_MAP.has_key(name):
+            if hasattr(context_expl, 'align_to_grid'):
+                context.manage_changeProperties({'align_to_grid': '0'})
+
+            if name not in VIEW_INTERFACES_MAP:
                 new = VIEW_INTERFACES_MAP['normal']
             else:
                 new = VIEW_INTERFACES_MAP[name]
@@ -142,25 +151,32 @@ class ChangeDesign(BrowserView):
                 if iface.providedBy(context):
                     noLongerProvides(context, iface)
             #set the new one
-            alsoProvides(context,new)
+            alsoProvides(context, new)
             context.reindexObject(idxs=['object_provides'])
-        
+
         #set init interfaces on blocks
         ifaces = []
-        if INIT_INTERFACES_MAP.has_key(name):
+        if name in INIT_INTERFACES_MAP:
             ifaces = INIT_INTERFACES_MAP[name]
-        
-        contents = blocks and blocks or context.getFolderContents({'object_provides':BLOCK_INTERFACES}, full_objects=True)
-        
+
+        contents = blocks and blocks or context.getFolderContents(
+            {'object_provides': BLOCK_INTERFACES},
+            full_objects=True)
+
         for obj in contents:
             #remove given block heights
             blockconf = IBlockConfig(obj)
             blockconf.block_height = None
-            
+
             #remove all related Interfaces
-            for i in COLUMN_INTERFACES_MAP.values()+SLOT_INTERFACES_MAP.values():
-                if i.providedBy(obj): noLongerProvides(obj,i)
-            for iface in ifaces: alsoProvides(obj, iface)
+            for i in COLUMN_INTERFACES_MAP.values()+ \
+                SLOT_INTERFACES_MAP.values():
+
+                if i.providedBy(obj):
+                    noLongerProvides(obj, i)
+
+            for iface in ifaces:
+                alsoProvides(obj, iface)
             obj.reindexObject(idxs=['object_provides'])
 
             #calc new images sizes and store them
@@ -169,20 +185,18 @@ class ChangeDesign(BrowserView):
             except ComponentLookupError:
                 pass
             converter.update(context, obj, obj.REQUEST)
-        
+
         if not blocks:
             return self.request.RESPONSE.redirect(context.absolute_url())
-    
-    
-    
+
+
 class SimpleLayoutControlsView(BrowserView):
 
     template = ViewPageTemplateFile("browser/controls.pt")
 
     def __call__(self):
         return self.template()
-        
-    
+
     def getActions(self, category='sl-actions'):
         types_tool = getToolByName(self.context, 'portal_types')
         m_tool = getToolByName(self.context, 'portal_membership')
@@ -190,7 +204,7 @@ class SimpleLayoutControlsView(BrowserView):
         member = m_tool.getAuthenticatedMember()
         for action in actions:
             has_permissions = True
-            if action.category == category:
+            if action.category == category and action.visible:
                 for permission in action.permissions:
                     if not member.has_permission(permission, self.context):
                         has_permissions = False
@@ -200,38 +214,41 @@ class SimpleLayoutControlsView(BrowserView):
                 econtext = getExprContext(self.context, self.context)
                 action = action.getAction(ec=econtext)
                 yield {
-                       'id':action['id'],
-                       'icon':action['icon'],
-                       'url':action['url']
-                       }
+                       'id': action['id'],
+                       'icon': action['icon'],
+                       'url': action['url']}
 
     def getLayouts(self, category='sl-layouts'):
         return self.getActions(category)
 
-    def getCurrentLayout(self,block):
+    def getCurrentLayout(self, block):
         if ISimpleLayoutBlock.providedBy(block):
             blockconf = IBlockConfig(block)
             viewname = blockconf.viewname
             image_layout = blockconf.image_layout
-            if not image_layout: image_layout = ''
-            if not viewname: viewname = ''
-            if viewname: viewname = '-%s' % viewname
+            if not image_layout:
+                image_layout = ''
+            if not viewname:
+                viewname = ''
+            if viewname:
+                viewname = '-%s' % viewname
             return  image_layout + viewname
 
         return None
 
     def isWorkflowEnabled(self):
         conf = getUtility(ISlUtils, name='simplelayout.utils')
-        return conf.isBlockWorkflowEnabled()    
-        
-    
+        return conf.isBlockWorkflowEnabled()
+
     def isTwoColumnLayout(self):
         context = aq_inner(self.context).aq_explicit
 
-        if ISimplelayoutTwoColumnView.providedBy(context):return True
-        if ISimplelayoutTwoColumnOneOnTopView.providedBy(context):return True
+        if ISimplelayoutTwoColumnView.providedBy(context):
+            return True
+        if ISimplelayoutTwoColumnOneOnTopView.providedBy(context):
+            return True
         return False
-        
+
     def ToggleGridLayoutText(self):
         """
         toggles align to grid text
@@ -242,6 +259,7 @@ class SimpleLayoutControlsView(BrowserView):
         grid = getattr(context.aq_inner.aq_explicit, 'align_to_grid', '0')
         return int(grid) and not_align_to_grid_text or align_to_grid_text
 
+
 class BlockManipulation(BrowserView):
     """
     """
@@ -249,17 +267,19 @@ class BlockManipulation(BrowserView):
     def setBlockHeights(self, uids=[], left=[], right=[]):
         """
         usage: needs a list of uid, height pairs...
-        if the uid of a block is missing, the height attribute of this 
+        if the uid of a block is missing, the height attribute of this
         block will be deleted
         """
         # create a dict
         _d = {}
-        real_uids = [u.replace('uid_','') for u in uids]
-        both = [[l.split(',')[0],l.split(',')[1]] for l in left] + [[r.split(',')[0],r.split(',')[1]] for r in right]
-        for uid,height in both:
-            _d[uid.replace('uid_','')] = height
-        
-        
+        real_uids = [u.replace('uid_', '') for u in uids]
+        both = [[l.split(',')[0], l.split(',')[1]] for l in left] + \
+            [[r.split(',')[0], r.split(',')[1]] for r in right]
+
+        for uid, height in both:
+            _d[uid.replace('uid_', '')] = height
+
+
         for uid in real_uids:
             block = self.context.reference_catalog.lookupObject(uid)
             blockconf = IBlockConfig(block)
@@ -269,5 +289,5 @@ class BlockManipulation(BrowserView):
             else:
                 #remove height
                 blockconf.block_height = None
-                
+
         return 1
