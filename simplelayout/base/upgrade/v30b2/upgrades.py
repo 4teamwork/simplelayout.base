4bf63f8e2bb3b59f2fa48f0273ecb6775ffc082a
Elio Schmutz <e.schmutz@4teamwork.ch>
2012-04-02 10:14:23 +0000

    add upgradestep to v3.0b2

---
+++ simplelayout/base/upgrade/v30b2/upgrades.py
@@ -0,0 +1,8 @@
+import transaction
+from plone.app.blob.migrations import migrate
+
+def change_paragraph_image_storage_type_to_blobstorage(context):
+    """Change the storagetype of the imagefield for paragraph"""
+
+    print migrate(context, 'Paragraph')
+    transaction.commit()
