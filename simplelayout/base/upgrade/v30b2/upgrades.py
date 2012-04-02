67a878298f7a0a0c637010815f3d94b1428ff968
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-04-02 11:57:26 +0000

    merged es_feature_blobstorage branch into trunk / changelog

--- simplelayout/base/upgrade/v30b2/upgrades.py
+++ simplelayout/base/upgrade/v30b2/upgrades.py
@@ -5,4 +5,5 @@ def change_paragraph_image_storage_type_to_blobstorage(context):
     """Change the storagetype of the imagefield for paragraph"""
 
     print migrate(context, 'Paragraph')
+    print migrate(context, 'News')
     transaction.commit()
